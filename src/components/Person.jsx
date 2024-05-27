e836047c0a4c5a3933ad2bf76b930d35ead5d28e
Juan C Muñoz <juancarlosmpdev@gmail.com>
2024-05-27 18:32:12 -0500

    fix

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -8,7 +8,6 @@ export const Person = (props) => {
           <div className="card-body">
             <h5 className="card-title">{props.name}</h5>
             <p className="card-text">{props.role}</p>
-            
           </div>
       </div>
     </div>
